b4099378883fe4f56437ff99a007d09bac2cdf7e
A-238 <agafumiya@gmail.com>
2025-04-30 03:02:16 +0900

    ファースト terraform init

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Terraform Docker Environment
+
+## Command
+
+#### format
+$ docker-compose run --rm terraform fmt --recursive
+
+#### init
+###### app/foobar
+$ docker-compose run --rm terraform -chdir=envs/stg/app/foobar init
---
+++ envs/stg/provider.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+    region = "ap-northeast-1"
+
+    default_tags {
+        tags = {
+            Env = "stg"
+            System = "example"
+        }
+    }
+}
+
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "5.96.0"
+        }
+    }
+
+    required_version = "1.11.4"
+}
