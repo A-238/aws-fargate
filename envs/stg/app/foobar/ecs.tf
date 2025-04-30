e4f9bae442074e479dea8c3fa2b6a4fde2f24fea
A-238 <agafumiya@gmail.com>
2025-04-30 22:12:02 +0900

    PHP用のECRを作成

--- envs/stg/app/foobar/ecs.tf
+++ envs/stg/app/foobar/ecs.tf
@@ -1,4 +1,9 @@
 module "nginx" {
   source = "../../../../modules/ecr"
-  name   = "example-stg-foobar-nginx"
+  name   = "${local.system_name}-${local.environment}-${local.service_name}-nginx"
+}
+
+module "php" {
+  source = "../../../../modules/ecr"
+  name   = "${local.system_name}-${local.environment}-${local.service_name}-php"
 }
---
+++ envs/stg/app/foobar/locals.tf
@@ -0,0 +1,5 @@
+locals {
+  service_name = "foobar"
+  environment  = "stg"
+  system_name  = "example"
+}
