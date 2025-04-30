d432fcfc81fa41d5c2fe0ddf3d10a97eb5f0a650
A-238 <agafumiya@gmail.com>
2025-04-30 21:23:46 +0900

    ECRライフサイクルポリシーを作成

--- envs/stg/app/foobar/ecs.tf
+++ envs/stg/app/foobar/ecs.tf
@@ -5,3 +5,25 @@ resource "aws_ecr_repository" "nginx" {
     Name = "example-stg-foobar-nginx"
   }
 }
+
+resource "aws_ecr_lifecycle_policy" "nginx" {
+  repository = aws_ecr_repository.nginx.name
+
+  policy = jsonencode({
+    "rules" : [
+      {
+        "rulePriority" : 1,
+        "description" : "Hold only 10 images, expire all others",
+        "selection" : {
+          "tagStatus" : "any",
+          "countType" : "imageCountMoreThan",
+          "countNumber" : 10
+        },
+        "action" : {
+          "type" : "expire"
+        }
+      }
+    ]
+  })
+
+}
