381328ce445b38a7707a7cdda7bcfdc9196b4a7f
A-238 <agafumiya@gmail.com>
2025-04-30 21:57:49 +0900

    ECRモジュール化

--- envs/stg/app/foobar/ecs.tf
+++ envs/stg/app/foobar/ecs.tf
@@ -1,29 +1,4 @@
-resource "aws_ecr_repository" "nginx" {
-  name = "example-stg-foobar-nginx"
-
-  tags = {
-    Name = "example-stg-foobar-nginx"
-  }
-}
-
-resource "aws_ecr_lifecycle_policy" "nginx" {
-  repository = aws_ecr_repository.nginx.name
-
-  policy = jsonencode({
-    "rules" : [
-      {
-        "rulePriority" : 1,
-        "description" : "Hold only 10 images, expire all others",
-        "selection" : {
-          "tagStatus" : "any",
-          "countType" : "imageCountMoreThan",
-          "countNumber" : 10
-        },
-        "action" : {
-          "type" : "expire"
-        }
-      }
-    ]
-  })
-
+module "nginx" {
+  source = "../../../../modules/ecr"
+  name   = "example-stg-foobar-nginx"
 }
